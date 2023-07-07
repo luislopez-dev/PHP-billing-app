fc1238c82bd193fc57a35dae8e18c5ab3b299a4a
Luis Lopez <luis.dev.master@gmail.com>
2023-07-07 08:44:06 -0600

    Cleaning code

--- App/Entities/Product.php
+++ App/Entities/Product.php
@@ -35,50 +35,97 @@ class Product
     #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id')]
     private int $category;
 
+    /**
+     * @return int
+     */
     public function getId(): int
     {
         return $this->id;
     }
+
+    /**
+     * @param int $id
+     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
+
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
+
+    /**
+     * @param string $name
+     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
+
+    /**
+     * @return string
+     */
     public function getDescription(): string
     {
         return $this->description;
     }
+
+    /**
+     * @param string $description
+     */
     public function setDescription(string $description): void
     {
         $this->description = $description;
     }
+
+    /**
+     * @return string
+     */
     public function getImgUrl(): string
     {
         return $this->imgUrl;
     }
+
+    /**
+     * @param string $imgUrl
+     */
     public function setImgUrl(string $imgUrl): void
     {
         $this->imgUrl = $imgUrl;
     }
+
+    /**
+     * @return int
+     */
     public function getStock(): int
     {
         return $this->stock;
     }
+
+    /**
+     * @param int $stock
+     */
     public function setStock(int $stock): void
     {
         $this->stock = $stock;
     }
+
+    /**
+     * @return float
+     */
     public function getPrice(): float
     {
         return $this->price;
     }
+
+    /**
+     * @param float $price
+     */
     public function setPrice(float $price): void
     {
         $this->price = $price;
